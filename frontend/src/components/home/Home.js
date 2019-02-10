22f4d6f8210b85e194b1bc39936ebdc2b81cb269
Gregory Merlet <gregory.merlet@outlook.fr>
2019-02-10 18:22:58 +0100

    Starting to create home page

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 import './Home.scss';
+import {Col, Container, Row} from "react-bootstrap";
 
 class Home extends React.Component {
     render() {
         return (
-            <div>
-                <div>
-                    Home
-                </div>
+            <div className="homeContent">
+                <Container className="homeContainer">
+                    <Row className="homeRow">
+                        <Col md={6} className="homeCol">
+                            <img src={require("../../assets/imgs/logo.png")} />
+                            <p className="textShadow">Elfy est une application vous permettant de vous renseigner sur les aliments que vous achetez afin d'améliorer votre consommation.</p>
+                        </Col>
+                        <Col md={6} className="homeCol">
+                            <h1 className="textShadow">Commencez dès maintenant !</h1>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
