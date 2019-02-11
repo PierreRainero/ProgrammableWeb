1bd2a6da38b50cfd8c98f0d35060c2b937ea9c57
Gaulthier Toussaint <gaulthiertoussaint@gmail.com>
2019-02-11 16:31:53 +0100

    add stores router, create find by id, find all, find by location

---
+++ backend/app/stores/location.js
@@ -0,0 +1,17 @@
+
+/**
+ * Class representing a location
+ */
+module.exports = class Location {
+
+    /**
+     * Normal constructor
+     * @param {number} lat Location lat
+     * @param {number} long Location long
+     */
+    constructor(lat, long) {
+        this.lat = lat;
+        this.long = long;
+    }
+
+}
