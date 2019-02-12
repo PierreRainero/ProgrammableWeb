e55d47531e869cc1ad0c7d97ad1ce24bed6257b3
Gregory Merlet <gregory.merlet@outlook.fr>
2019-02-12 20:37:06 +0100

    Adding Recipes and Prices to Product Screen

--- frontend/src/components/product/product-screen/ProductScreen.js
+++ frontend/src/components/product/product-screen/ProductScreen.js
@@ -3,7 +3,7 @@ import Loading from '../../loading/Loading';
 import ProductService from '../ProductService';
 import ProductScore from '../product-score/ProductScore';
 import CardList from './cardList/CardList';
-import { Col, Container } from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
 
 import './ProductScreen.scss';
 
@@ -48,15 +48,15 @@ class ProductScreen extends React.Component {
      */
     render() {
         return (
-            <div>
+            <div style={{height: '100%'}}>
                 {this.state.loading ?
                     <div>
                         <Loading />
                     </div>
                     :
-                    <div>
+                    <div style={{height: '100%'}}>
                         <div className='productHeader'>
-                            <img src='http://lorempixel.com/1920/300/food' alt='header' />
+                            <img src='http://lorempixel.com/1920/250/food' alt='header' />
                         </div>
                         <div className='productGeneralInfos'>
                             <div className='productImage'>
@@ -71,16 +71,29 @@ class ProductScreen extends React.Component {
                                 <ProductScore score={this.state.product.score} nutrigrade={this.state.product.nutrigrade} novaGroup={this.state.product.novaGroup} />
                             </div>
                         </div>
+                        <div>
+
+                        </div>
                         <Container className='productDetails'>
-                            <Col md={4}>
-                                <CardList title='Ingrédients' data={this.state.product.ingredients} />
-                            </Col>
-                            <Col md={4}>
-                                <CardList title='Additifs' data={this.state.product.additives} />
-                            </Col>
-                            <Col md={4}>
-                                <CardList title='Allergènes' data={this.state.product.allergens} />
-                            </Col>
+                            <Row className='productDetailsRow'>
+                                <Col md={4}>
+                                    <CardList title='Ingrédients' data={this.state.product.ingredients} />
+                                </Col>
+                                <Col md={4}>
+                                    <CardList title='Additifs' data={this.state.product.additives} />
+                                </Col>
+                                <Col md={4}>
+                                    <CardList title='Allergènes' data={this.state.product.allergens} />
+                                </Col>
+                            </Row>
+                            <Row className='productDetailsRow'>
+                                <Col md={6}>
+                                    <CardList title='Recettes' data={[]} />
+                                </Col>
+                                <Col md={6}>
+                                    <CardList title='Comparaison des prix' data={[]} />
+                                </Col>
+                            </Row>
                         </Container>
                     </div>
                 }
